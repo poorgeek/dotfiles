4955620440683b2953bebfa4bcf679d4ee215cfc
Justin Stockton <poorgeek@gmail.com>
2012-11-13 22:24:46 -0700

    update with my own configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,14 +8,18 @@ function notice { echo -e "\e[01;34m=> $1\e[00m"; }
 function error { echo -e "\e[00;31m=> Error: $1\e[00m"; }
 
 cd "$(dirname "$0")"
+
 notice "Updating repo.."
 git pull --rebase origin master
 git submodule init
 git submodule update
+
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) "
 echo -e "\e[01;34m=> Installing files..\e[00m"
+
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   CWD=`pwd`
+
   excludes=".git gitprompt sublime2 .DS_Store bootstrap.sh README.md"
   for file in `ls -A`
   do
@@ -34,15 +38,17 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   ln -sf $CWD/gitprompt/git-prompt.sh $HOME/.git-prompt.sh
 
   SUBL_SRC="$CWD/sublime2"
-  SUBL_TARGET="$HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages/User"
+  SUBL_HOME="$HOME/Library/Application Support/Sublime Text 2/Packages/User"
   for file in `ls -A $SUBL_SRC`
   do
-    if [ -e $SUBL_TARGET/$file ]
+    if [ -e "$SUBL_HOME/$file" ]
     then
-      rm -rf $SUBL_TARGET/$file
+      rm -rf "$SUBL_HOME/$file"
     fi
-    ln -sf $SUBL_SRC/$file $SUBL_TARGET/$file
+    ln -sf "$SUBL_SRC/$file" "$SUBL_HOME/$file"
   done
 fi
+
 source "$HOME/.bash_profile"
+
 notice "Done.."
